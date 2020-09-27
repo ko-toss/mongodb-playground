6d6cf330de2bb5f12fd8067639055c862843c3e5
ko-toss <55690750+ko-toss@users.noreply.github.com>
2020-09-27 13:20:58 +0900

    setup and test out mongodb connection (#5)
    * add mongodb nodejs driver
    
    * add mongodb connection(with cache) utility:
    - copy pasted from with-mongodb
    
    * test out mongodb connection.
    - MONGODB_URI and MONGODB_DB env should be set at .env.local and vercel environment.

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from 'next'
+import { connectToDatabase } from 'utils/mongodb';
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { db } = await connectToDatabase()
+  const movies = await db
+    .collection("movies")
+    .find({})
+    .sort({ metacritic: -1 })
+    .limit(20)
+    .toArray();
+  res.json(movies);
 }
