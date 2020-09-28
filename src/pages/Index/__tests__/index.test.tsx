24c637ed8008695395fbaffd00ebe1a9e918fa7d
Seunghoon Ko <ko@toss.im>
2020-09-29 02:24:13 +0900

    fix test by passing real store for now

--- src/pages/Index/__tests__/index.test.tsx
+++ src/pages/Index/__tests__/index.test.tsx
@@ -2,6 +2,8 @@ import { getSomeData } from 'remotes/getSomeData';
 import { mocked } from 'ts-jest/utils';
 import { fireEvent, render, waitFor } from '@testing-library/react';
 import { IndexPage } from 'pages/Index/index';
+import { Provider } from 'react-redux';
+import { reduxStore } from 'store';
 
 jest.mock('remotes/getSomeData')
 
@@ -10,7 +12,11 @@ const mockedGetSomeData = mocked(getSomeData);
 describe('Describe what we are testing, ', () => {
   test('with details', async () => {
     mockedGetSomeData.mockReturnValue(Promise.resolve('Some fake data received'))
-    const { getByText } = render(<IndexPage />)
+    const { getByText } = render(
+      <Provider store={reduxStore}>
+        <IndexPage />
+      </Provider>
+    )
 
     fireEvent.click(getByText('Some Button'))
 
