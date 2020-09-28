7245ce25919bf2148005441843514284c359c7dc
Seunghoon Ko <ko@toss.im>
2020-09-29 02:19:28 +0900

    add dispatch to component

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,7 +1,14 @@
 import { useCallback, useState } from 'react';
 import { getSomeData } from 'remotes/getSomeData';
+import { useDispatch, useSelector } from 'react-redux';
+import { someAction, someEpicAction } from 'store/actions/someActions';
+import { selectSomeFeatureEpicReducerValue, selectSomeFeatureReducerValue } from 'store/selectors/someSelector';
 
 export function IndexPage() {
+  const dispatch = useDispatch();
+  const someFeatureValue = useSelector(selectSomeFeatureReducerValue);
+  const someEpicFeatureValue = useSelector(selectSomeFeatureEpicReducerValue)
+
   const [data, setData] = useState('');
 
   const handleClicked = useCallback(async () => {
@@ -13,5 +20,11 @@ export function IndexPage() {
     Index Page
     <button onClick={handleClicked}>Some Button</button>
     <div>{data}</div>
+
+    <button onClick={() => dispatch(someAction(100))}>Redux Button</button>
+    <div>{someFeatureValue}</div>
+
+    <button onClick={() => dispatch(someEpicAction(100))}>Redux Epic Button</button>
+    <div>{someEpicFeatureValue}</div>
   </div>
 }
