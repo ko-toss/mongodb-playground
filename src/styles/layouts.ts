7bcb277036f6ec31175f2a91a7b07aa70301333f
Seunghoon Ko <ko@toss.im>
2020-09-29 14:50:15 +0900

    implement commonly used styles

---
+++ src/styles/layouts.ts
@@ -0,0 +1,18 @@
+import { css } from '@emotion/core';
+
+export const cssPaddingH = css`
+  padding-left: 24px;
+  padding-right: 24px;
+`
+
+export const cssMarginH = css`
+  margin-left: 24px;
+  margin-right: 24px;
+`
+
+export const cssCheckBox = css`
+  width: 18px;
+  height: 18px;
+  border-radius: 50%;
+  border: 1px solid lightgrey;
+`
